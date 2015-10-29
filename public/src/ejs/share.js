e968a34712cb4797d13d69fdeaf6fe49689ea147
svon <svon@svon.org>
2015-10-29 15:21:14 +0800

    test9

---
+++ public/src/ejs/share.js
@@ -0,0 +1,39 @@
+$(function() {
+    $.get(
+        "/config", {
+            url: location.href.split('#')[0]
+        },
+        function(resp) {
+            wx.config({
+                debug: true,
+                appId: resp.appid,
+                timestamp: resp.timestamp,
+                nonceStr: resp.nonceStr,
+                signature: resp.signature,
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo'
+                ]
+            });
+            // 微信接口调用
+            wx.ready(function() {
+                var shareData = {
+                    title: '在路上',
+                    desc: '在路上、博客',
+                    link: 'http://svon.org',
+                    imgUrl: "http://static.nodejs.so/sns.png",
+                    success: function() {
+
+                    }
+                };
+                wx.onMenuShareTimeline(shareData);
+                wx.onMenuShareAppMessage(shareData);
+                wx.onMenuShareQQ(shareData);
+                wx.onMenuShareWeibo(shareData);
+            });
+        }
+    );
+});
