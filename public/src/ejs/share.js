100fe1b3b062aacbfd6b3a79a937048538b3331a
svon <svon@svon.org>
2015-11-24 00:11:22 +0800

    fix

--- public/src/ejs/share.js
+++ public/src/ejs/share.js
@@ -1,37 +1,54 @@
-+function(){
-    if(!window['wx']){
-        return false;
-    }
-    wxShare(function(resp){
-        wx.config({
-            debug: false,
-            appId: resp.appid,
-            timestamp: resp.timestamp,
-            nonceStr: resp.nonceStr,
-            signature: resp.signature,
-            jsApiList: [
-                'checkJsApi',
-                'onMenuShareTimeline',
-                'onMenuShareAppMessage',
-                'onMenuShareQQ',
-                'onMenuShareWeibo'
-            ]
-        });
-        // 微信接口调用
-        wx.ready(function() {
-            var shareData = {
-                title: '在路上',
-                desc: '在路上、博客',
-                link: 'http://wx.nodejs.so',
-                imgUrl: "http://static.nodejs.so/sns.png",
-                success: function() {
++ function() {
+    // if (!window['wx']) {
+    //     return false;
+    // }
+    window.wxShare = new (function() {
+        var obj = {
+            title: '在路上',
+            desc: '在路上、博客',
+            link: 'http://wx.nodejs.so',
+            imgUrl: "http://static.nodejs.so/sns.png"
+        };
+        this.setTitle = function(value){
 
-                }
-            };
-            wx.onMenuShareTimeline(shareData);
-            wx.onMenuShareAppMessage(shareData);
-            wx.onMenuShareQQ(shareData);
-            wx.onMenuShareWeibo(shareData);
-        });
-    });
+        };
+        this.setDesc = function(value){
+
+        };
+        this.setLink = function(value){
+
+        };
+        this.setSns = function(value){
+
+        };
+        this.ready = function(resp) {
+            wx.config({
+                debug: true,
+                appId: resp.appid,
+                timestamp: resp.timestamp,
+                nonceStr: resp.nonceStr,
+                signature: resp.signature,
+                jsApiList: [
+                    'checkJsApi',
+                    'onMenuShareTimeline',
+                    'onMenuShareAppMessage',
+                    'onMenuShareQQ',
+                    'onMenuShareWeibo'
+                ]
+            });
+            // 微信接口调用
+            wx.ready(function() {
+                var shareData = $.extend(obj,{
+                    success: function() {
+
+                    }
+                });
+                wx.onMenuShareTimeline(shareData);
+                wx.onMenuShareAppMessage(shareData);
+                wx.onMenuShareQQ(shareData);
+                wx.onMenuShareWeibo(shareData);
+            });
+        };
+        return this;
+    })();
 }();
--- wechat/share.js
+++ wechat/share.js
@@ -1,11 +1,11 @@
 (function (resp) {
-    //Created by svon on 2015/11/2.
-    window.wxShare = function (callback) {
-        callback(resp);
-    };
-    var date = new Date();
-    var hm = document.createElement("script");
-    hm.src = "//static.nodejs.so/share.js?date=" + date.getFullYear()+"-"+(date.getMonth() + 1)+"-"+date.getDate();
-    var s = document.getElementsByTagName("head")[0];
-    s.appendChild(hm);
+  window.wxShareConfig = function(callback){
+    callback(resp);
+  };
+  try{
+    window.wxShare && window.wxShare.ready(resp);
+  }
+  catch(e){
+
+  }
 })(arguments[0]);
